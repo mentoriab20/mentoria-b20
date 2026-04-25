3fd9578b2a0b2dfd6ad0e6161767479117462e43
mentoriab20 <mentoriab20@gmail.com>
2026-04-25 01:40:23 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,6 +2,26 @@ const https = require('https');
 
 const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwAQFrSuwnW0MB1-9I8byO3taexGc9NIVvkwDr00IVYg-YEMx7EaF3vu5T_APZ1FMPHhQ/exec';
 
+function fetchFollow(url, redirectCount = 0) {
+  return new Promise((resolve, reject) => {
+    if (redirectCount > 5) return reject(new Error('Too many redirects'));
+
+    https.get(url, (res) => {
+      // Segue redirect automaticamente
+      if (res.statusCode >= 300 && res.statusCode < 400 && res.headers.location) {
+        return fetchFollow(res.headers.location, redirectCount + 1)
+          .then(resolve)
+          .catch(reject);
+      }
+
+      let body = '';
+      res.on('data', chunk => body += chunk);
+      res.on('end', () => resolve(body));
+      res.on('error', reject);
+    }).on('error', reject);
+  });
+}
+
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
@@ -18,18 +38,9 @@ module.exports = async (req, res) => {
   const url = SCRIPT_URL + (query ? '?' + query : '');
 
   try {
-    const data = await new Promise((resolve, reject) => {
-      https.get(url, (response) => {
-        let body = '';
-        response.on('data', chunk => body += chunk);
-        response.on('end', () => resolve(body));
-        response.on('error', reject);
-      }).on('error', reject);
-    });
-
+    const data = await fetchFollow(url);
     res.setHeader('Content-Type', 'application/json');
     res.status(200).send(data);
-
   } catch (err) {
     res.status(500).json({ erro: err.message });
   }
